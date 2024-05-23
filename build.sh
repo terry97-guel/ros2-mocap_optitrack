9013688692e7b8e2575a378a6f2d48a1604976ea
terry <taerimyoon@korea.ac.kr>
2024-05-23 22:55:09 +0900

    mocap success

--- build.sh
+++ build.sh
@@ -4,13 +4,18 @@
 source /opt/ros/foxy/setup.bash
 
 # Initialize flags
-REMOVE_FLAG=false
+LOCAL_FLAG=false
+ONBOARD_FLAG=false
 
 # Parse command-line arguments
 for arg in "$@"; do
 	case $arg in
-		--rm)
-			REMOVE_FLAG=true
+		--l)
+			LOCAL_FLAG=true
+			shift
+			;;
+		--b)
+			ONBOARD_FLAG=true
 			shift
 			;;
 		*)
@@ -21,14 +26,36 @@ for arg in "$@"; do
 done
 
 # Handle --rm flag
-if [ "$REMOVE_FLAG" = true ]; then
-	echo "INFO::removing exisiting build"
+# if [ "$REMOVE_FLAG" = true ]; then
+# 	echo "INFO::removing exisiting build"
+# 	rm -rf build
+# 	rm -rf install
+# 	rm -rf log
+# fi
+
+# colcon build --packages-select mocap_optitrack_interfaces mocap_optitrack_client
+# . install/setup.bash
+
+if [ "$ONBOARD_FLAG" = true ]; then
 	rm -rf build
 	rm -rf install
+	rm -rf log
+
+	# Touching solves timing problem somehow (?)
+	find /opt/ros/ -exec touch {} +
+	find /usr/include/python3.8 -exec touch {} +
+	find /home/isaac/go1_gym -exec touch {} +
+	colcon build --packages-select mocap_optitrack_interfaces 
+	. install/setup.bash
 fi
 
-colcon build --packages-select mocap_optitrack_interfaces mocap_optitrack_client
-. install/setup.bash
+if [ "$LOCAL_FLAG" = true ]; then
+	colcon build --packages-select mocap_optitrack_interfaces mocap_optitrack_client
+	. install/setup.bash
+	# Launch the ROS2 application
+	ros2 launch launch/launch_mocap.py
+fi
 
-# Launch the ROS2 application
-ros2 launch launch/launch_mocap.py
+echo "INFO:: sourcing" # Somehow sourcing does not work in onboard docker 
+source /opt/ros/foxy/setup.bash
+. install/setup.bash
