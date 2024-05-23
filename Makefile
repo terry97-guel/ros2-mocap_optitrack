528eab787c3adc513dc05dca3a2f254f941610d5
terry <taerimyoon@korea.ac.kr>
2024-05-23 20:03:38 +0900

    build script takes flag

---
+++ Makefile
@@ -0,0 +1,16 @@
+default: host
+host_scratch:
+		rm -rf build
+		rm -rf install
+		source /opt/ros/foxy/setup.bash
+		colcon build --packages-select mocap_optitrack_interfaces mocap_optitrack_client mocap_optitrack_w2b
+		. install/setup.bash
+		ros2 launch launch/launch_y_up.py
+
+host:
+		rm -rf build
+		rm -rf install
+		source /opt/ros/foxy/setup.bash
+		colcon build --packages-select mocap_optitrack_interfaces mocap_optitrack_client mocap_optitrack_w2b
+		. install/setup.bash
+		ros2 launch launch/launch_y_up.py
--- build.sh
+++ build.sh
@@ -1,8 +1,45 @@
-rm -rf build
-rm -rf install
+#!/bin/bash
 
+# Source ROS Foxy setup script
 source /opt/ros/foxy/setup.bash
-colcon build --packages-select mocap_optitrack_interfaces mocap_optitrack_client mocap_optitrack_w2b
-. install/setup.bash
 
+# Initialize flags
+REMOVE_FLAG=false
+ALL_FLAG=false
+
+# Parse command-line arguments
+for arg in "$@"; do
+	case $arg in
+		--rm)
+			REMOVE_FLAG=true
+			shift
+			;;
+		--all)
+			ALL_FLAG=true
+			shift
+			;;
+		*)
+			echo "Unknown option: $arg"
+			exit 1
+			;;
+	esac
+done
+
+# Handle --rm flag
+if [ "$REMOVE_FLAG" = true ]; then
+	echo "INFO::removing exisiting build"
+	rm -rf build
+	rm -rf install
+fi
+
+# Handle --all flag
+if [ "$ALL_FLAG" = true ]; then
+	colcon build --packages-select mocap_optitrack_interfaces mocap_optitrack_client mocap_optitrack_w2b
+	. install/setup.bash
+else
+	colcon build --packages-select mocap_optitrack_interfaces 
+	. install/setup.bash
+fi
+
+# Launch the ROS2 application
 ros2 launch launch/launch_y_up.py
